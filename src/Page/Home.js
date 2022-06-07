79d6d8ffbc10c46b4c56185810ca1fa60d9b21ec
julymoe <athelay.lwin@gmail.com>
2022-06-07 10:24:33 +0630

    remove svg from home

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -26,50 +26,6 @@ export default function Home() {
       >
         Home
       </motion.h1>
-      <motion.svg
-        width="600"
-        height="600"
-        viewBox="0 0 600 600"
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.circle
-          cx="100"
-          cy="100"
-          r="80"
-          stroke="#ff0055"
-          variants={draw}
-          custom={1}
-        />
-        <motion.line
-          x1="220"
-          y1="30"
-          x2="360"
-          y2="170"
-          stroke="#00cc88"
-          variants={draw}
-          custom={2}
-        />
-        <motion.line
-          x1="220"
-          y1="170"
-          x2="360"
-          y2="30"
-          stroke="#00cc88"
-          variants={draw}
-          custom={2.5}
-        />
-        <motion.rect
-          width="140"
-          height="140"
-          x="410"
-          y="30"
-          rx="20"
-          stroke="#0099ff"
-          variants={draw}
-          custom={3}
-        />
-      </motion.svg>
     </div>
   );
 }
